aa5d4b5a3bec6042b19c3252dbee620903b842ea
Viktoria Bogatyreva <bogatyreva-va@mail.ru>
2024-05-20 17:27:08 +0300

    2.2 MVVM - added branch master

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -8,6 +8,8 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import ru.netology.nmedia.databinding.NetologyMainBinding
 
+// branch master
+
 class MainActivity : AppCompatActivity() {
 
      override fun onCreate(savedInstanceState: Bundle?) {
