5e700d18399149c50572cc3ca69d7a98adae5690
Viktoria Bogatyreva <bogatyreva-va@mail.ru>
2025-04-28 18:00:20 +0300

    Architecture Components.Part 1 - update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.3.0")
+        classpath("com.android.tools.build:gradle:8.4.0")
         classpath("com.google.gms:google-services:4.4.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.51")
